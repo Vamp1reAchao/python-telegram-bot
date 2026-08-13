2bfb223cac409134fecfed6556b8c54408b3c71e
kislit <132556711+Vamp1reAchao@users.noreply.github.com>
2026-08-13 21:23:37 +0800

    Complete builder wrapper

--- src/telegram/ext/_applicationbuilder_persistence_retry.py
+++ src/telegram/ext/_applicationbuilder_persistence_retry.py
@@ -1,2 +1,10 @@
 from telegram.ext._application_persistence_retry import Application
 from telegram.ext._applicationbuilder import ApplicationBuilder as _ApplicationBuilder
+
+
+class ApplicationBuilder(_ApplicationBuilder):
+    """Application builder using the persistence-safe Application by default."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.application_class(Application)
