20538b5b654f5fe3f959f624991516a5ff88337f
kislit <132556711+Vamp1reAchao@users.noreply.github.com>
2026-08-13 21:25:03 +0800

    Add persistence retry test imports

--- tests/ext/persistence_retry_regression.py
+++ tests/ext/persistence_retry_regression.py
@@ -1,2 +1,4 @@
-def test_persistence_retry_placeholder():
-    assert True
+import pytest
+
+from telegram.ext import ApplicationBuilder, PersistenceInput
+from tests.ext.test_basepersistence import TrackingPersistence
